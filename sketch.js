fd15773a79a09a2a2ef6df431b708fdc6723be2f
willi4mjoseph <00001089980176sp@al.educacao.sp.gov.br>
2024-06-15 14:23:10 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+  background("black"); 
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+  if(mouseIsPressed){
+    rect(mouseX, mouseX, 20, 35);
+      }
+  
+   }
